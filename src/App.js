6a993e73a65c27c74d449d77b59899ef60c2de30
Tamara <tamara.northe@laparkan.com>
2023-12-10 15:23:00 -0400

    3 new files and deleted a file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import { useEffect } from 'react';
 import './App.css';
+import SearchIcon from './search.svg';
 
 const API_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=8d42927f";
+
+const movie1 = {
+ 
+    "Title": "Spiderman",
+    "Year": "2010",
+    "imdbID": "tt1785572",
+    "Type": "movie",
+    "Poster": "N/A"
+
+}
 
 const App = () => {
 
@@ -16,7 +27,29 @@ const App = () => {
   }, [])
 
   return (
-    <h1>App</h1>
+    <div className='app'>
+      <h1>MovieLand</h1>
+      <div className='search'>
+        <input 
+          placeholder='Search for movies'
+          value='Superman'
+          onChange={() => {}}
+        />
+        <img
+          src={SearchIcon}
+          alt='search'
+          onClick={() => {}}
+        />
+      </div>
+
+      <div className='container'>
+        <div className='movie'>
+          <div>
+            <p>{movie1.Year}</p>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
